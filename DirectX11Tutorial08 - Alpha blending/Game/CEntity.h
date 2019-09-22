fa75354d7bf2f283b23530404484be86433b41f7
Jesus Kim <42127220+principal6@users.noreply.github.com>
2019-09-22 18:01:22 +0900

    Tutorial#08 - Alpha blending added
    CDirectX now has blend state for transparent image (such as alpha-channelled png)
    CDirectX::BeginRendering() now sets blend state as well

---
+++ DirectX11Tutorial08 - Alpha blending/Game/CEntity.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "../Helper/CObject2D.h"
+#include "../Helper/CTexture.h"
+
+class CEntity
+{
+	friend class CEntityPool;
+
+public:
+	CEntity(ID3D11Device* Device, ID3D11DeviceContext* DeviceContext) : m_Device{ Device }, m_DeviceContext{ DeviceContext } {};
+	~CEntity() {};
+
+	void CreateRectangle(const XMFLOAT2& RectangleSize);
+
+	void AddTexture(const string& TextureFileName);
+	CTexture* GetTexture(size_t Index);
+
+	void Draw();
+
+public:
+	ESampler				Sampler{ ESampler::Point };
+	XMFLOAT2				WorldPosition{};
+	float					RotationAngle{};
+	XMFLOAT2				Scalar{ 1.0f, 1.0f };
+
+private:
+	ID3D11Device*			m_Device{};
+	ID3D11DeviceContext*	m_DeviceContext{};
+
+	vector<CObject2D>		m_vObject2Ds{};
+	vector<CTexture>		m_vTextures{};
+
+	XMMATRIX				m_MatrixWorld{ XMMatrixIdentity() };
+};
